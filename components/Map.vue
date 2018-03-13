97468ecd4cff0e176a68da890a142e3c2d7c530d
Rasmus Schlünsen <>
2018-03-13 15:25:52 +0100

    Initial commit

---
+++ components/Map.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <div id="map-wrap" v-if="browser">
+    <v-map :zoom=4
+           :center="[markers[0].lat, markers[0].lng]">
+      <v-tilelayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></v-tilelayer>
+      <v-marker v-for="marker in markers" :key="marker.id" :lat-lng="[marker.lat, marker.lng]"></v-marker>
+    </v-map>
+  </div>
+</div>
+</template>
+
+<script>
+  export default {
+    ssr: false,
+    data() {
+      return {
+        browser: process.browser,
+        markers: [ 
+          {id: 1, lat: 55.8350812, lng: 8.1634942 },
+          {id: 2, lat: 47.413220, lng: -1.229482 }
+        ]
+      }
+
+    }
+  }
+</script>
+<style>
+  #map-wrap {
+    height: 100%;
+    width: 100%;
+  }
+  
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,57 @@
+<template>
+<div>
+  <Map class="map"></Map>
+  <div class="overlay ">
+    <section class="hero is-primary is-absolute">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">nuxt-leaflet</h1>
+          <h2 class="subtitle">
+            yarn add nuxt-leaflet
+      </h2>
+        </div>
+      </div>
+    </section>
+  </div>
+</div>
+
+</template>
+
+<script>
+import Map from '~/components/Map.vue';
+
+export default {
+  components: {
+    Map
+  }
+}
+
+</script>
+
+<style>
+.overlay {
+  position: absolute;
+  
+  z-index: 2;
+  width: 100%;
+  bottom: 0px;
+}
+
+.is-vcentered {
+  justify-content: center;
+  align-items: center;
+}
+
+.hero {
+  bottom: 0px;
+  position: absolute;
+  width: 100%;
+}
+
+.map {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  z-index: 1;
+}
+</style>
